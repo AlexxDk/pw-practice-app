3db119fd9c05a08183361681da0828c4ad4e256d
AlexxDk <dkalexandr@gmail.com>
2024-05-22 12:34:15 +0300

    user facing locators - getBY

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -8,7 +8,7 @@ test.beforeEach(async ({ page }) => {
 
 test("Locator syntax rules", async ({ page }) => {
   // by Tag name
-  await page.locator("input").click();
+  await page.locator("input").first().click();
 
   //by ID
   page.locator("#inputEmail");
@@ -35,4 +35,20 @@ test("Locator syntax rules", async ({ page }) => {
 
   //by exact text match
   page.locator(':text-is("Using the Grid")');
+});
+
+test("User facing locators", async ({ page }) => {
+  await page.getByRole("textbox", { name: "Email" }).first().click();
+  await page.getByRole("button", { name: "Sign in" }).first().click();
+
+  await page.getByLabel("Email").first().click();
+
+  await page.getByPlaceholder("Jane Doe").click();
+
+  await page.getByText("Using the Grid").click();
+
+  await page.getByTestId("SignIn").click();
+
+  // await page.getByTitle("IoT Dashboard").click();
+
 });
