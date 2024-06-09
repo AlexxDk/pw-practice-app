d76e4b2dbaf63523aad83bd19567a38ff65184e1
AlexxDk <dkalexandr@gmail.com>
2024-06-09 18:02:24 +0300

    radio buttons

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -15,15 +15,56 @@ test.describe("Form Layouts page", () => {
       .locator("nb-card", { hasText: "Using the Grid" })
       .getByRole("textbox", { name: "Email" });
 
-      await usingTheGridEmailInput.fill('test@test.com');
-      await usingTheGridEmailInput.clear();
-      await usingTheGridEmailInput.pressSequentially("test2@test.com", {delay: 200});
+    await usingTheGridEmailInput.fill("test@test.com");
+    await usingTheGridEmailInput.clear();
+    await usingTheGridEmailInput.pressSequentially("test2@test.com", {
+      delay: 200,
+    });
 
-      //generic assertion
-      const inputValue = await usingTheGridEmailInput.inputValue();
-      expect(inputValue).toEqual("test2@test.com")
+    //generic assertion
+    const inputValue = await usingTheGridEmailInput.inputValue();
+    expect(inputValue).toEqual("test2@test.com");
 
-      //locator assection
-      await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
+    //locator assertion
+    await expect(usingTheGridEmailInput).toHaveValue("test2@test.com");
+  });
+
+  test("radio button", async ({ page }) => {
+    const usingTheGridEmailInput = page.locator("nb-card", {
+      hasText: "Using the Grid",
+    });
+
+    // await usingTheGridEmailInput.getByLabel("Option 1").check({ force: true });
+    await usingTheGridEmailInput
+      .getByRole("radio", { name: "Option 1" })
+      .check({ force: true });
+    const radioStatus = await usingTheGridEmailInput
+      .getByRole("radio", { name: "Option 1" })
+      .isChecked();
+
+    // generic assertion
+    expect(radioStatus).toBeTruthy();
+
+    //locator assertion
+    await expect(
+      usingTheGridEmailInput.getByRole("radio", { name: "Option 1" })
+    ).toBeChecked();
+
+    await usingTheGridEmailInput
+      .getByRole("radio", { name: "Option 2" })
+      .check({ force: true });
+
+    //generic assertion
+    expect(
+      await usingTheGridEmailInput
+        .getByRole("radio", { name: "Option 1" })
+        .isChecked()
+    ).toBeFalsy();
+
+    expect(
+      await usingTheGridEmailInput
+        .getByRole("radio", { name: "Option 2" })
+        .isChecked()
+    ).toBeTruthy();
   });
 });
